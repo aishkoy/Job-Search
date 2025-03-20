1b4b9fd2e599c67437125c585acd363f51e1bcb1
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-03-20 17:35:33 +0600

    feat: created ResumeDao to work with resumes table in database

---
+++ src/main/java/kg/attractor/job_search/dao/ResumeDao.java
@@ -0,0 +1,16 @@
+package kg.attractor.job_search.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ResumeDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final KeyHolder keyHolder = new GeneratedKeyHolder();
+}
