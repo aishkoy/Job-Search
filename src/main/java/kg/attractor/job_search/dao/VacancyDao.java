45a27c75ecb385c57daa4b997f5ff8bf2ffd44b2
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-03-20 18:49:36 +0600

    feat: added methods for retrieving vacancies in the VacancyDao class:       - getVacanciesAppliedByUserId()       - getActiveVacancies()       - getVacancies()       - getVacanciesByCategoryId(Long categoryId)       - getVacanciesByCategoryName(String categoryName)       - getVacancyById(Long vacancyId)       - getVacanciesByEmployerId(Long employerId)

--- src/main/java/kg/attractor/job_search/dao/VacancyDao.java
+++ src/main/java/kg/attractor/job_search/dao/VacancyDao.java
@@ -1,11 +1,60 @@
 package kg.attractor.job_search.dao;
 
+import kg.attractor.job_search.mapper.dao.VacancyDaoMapper;
+import kg.attractor.job_search.models.Vacancy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class VacancyDao {
     private final JdbcTemplate jdbcTemplate;
+
+    public List<Vacancy> getVacanciesAppliedByUserId(Long userId) {
+        String sql = """
+                select * from vacancies v
+                inner join responded_applicants ra on ra.vacancy_id = v.id
+                inner join resumes r on ra.resume_id = r.id
+                where r.applicant_id = ?
+                """;
+        return jdbcTemplate.query(sql, new VacancyDaoMapper(), userId);
+    }
+
+    public List<Vacancy> getActiveVacancies(){
+        String sql = "select * from vacancies where IS_ACTIVE = true";
+        return jdbcTemplate.query(sql, new VacancyDaoMapper());
+    }
+
+    public List<Vacancy> getVacancies(){
+        String sql = "select * from vacancies";
+        return jdbcTemplate.query(sql, new VacancyDaoMapper());
+    }
+
+    public List<Vacancy> getVacanciesByCategoryId(Long categoryId) {
+        String sql = "select * from vacancies where category_id = ?";
+        return jdbcTemplate.query(sql, new VacancyDaoMapper(), categoryId);
+    }
+
+    public List<Vacancy> getVacanciesByCategoryName(String categoryName) {
+        String sql = """
+                select * from vacancies v
+                inner join categories c on v.category_id = c.id
+                where c.NAME like ?""";
+        return jdbcTemplate.query(sql, new VacancyDaoMapper(), categoryName + "%");
+    }
+
+    public Optional<Vacancy> getVacancyById(Long vacancyId) {
+        String sql = "select * from vacancies where id = ?";
+        Vacancy vacancy = jdbcTemplate.queryForObject(sql, new VacancyDaoMapper(), vacancyId);
+        return Optional.ofNullable(vacancy);
+    }
+
+    public List<Vacancy> getVacanciesByEmployerId(Long employerId) {
+        String sql = "select * from vacancies where AUTHOR_ID = ?";
+        return jdbcTemplate.query(sql, new VacancyDaoMapper(), employerId);
+    }
 }
