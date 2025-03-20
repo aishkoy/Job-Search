f98ad3c4b560303483a25e176c7ce717d7267b6f
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-03-20 18:05:15 +0600

    feat: added methods for retrieving resumes by different criteria:
    getResumeByCategoryId, getResumeByApplicantId, getResumeByApplicantName, getResumeById

--- src/main/java/kg/attractor/job_search/dao/ResumeDao.java
+++ src/main/java/kg/attractor/job_search/dao/ResumeDao.java
@@ -1,16 +1,39 @@
 package kg.attractor.job_search.dao;
 
+import kg.attractor.job_search.mapper.dao.ResumeDaoMapper;
+import kg.attractor.job_search.models.Resume;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ResumeDao {
     private final JdbcTemplate jdbcTemplate;
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private final KeyHolder keyHolder = new GeneratedKeyHolder();
+    public List<Resume> getResumesByCategoryId(Long categoryId) {
+        String sql = "select * from resumes where category_id = ?";
+        return jdbcTemplate.query(sql, new ResumeDaoMapper(), categoryId);
+    }
+
+    public List<Resume> getResumesByApplicantId(Long userId) {
+        String sql = "select * from resumes where applicant_id = ?";
+        return jdbcTemplate.query(sql, new ResumeDaoMapper(), userId);
+    }
+
+    public List<Resume> getResumesByApplicantName(String applicantName) {
+        String sql = """
+                select * from resumes r
+                inner join users u on r.applicant_id = u.id
+                where u.name like ?""";
+        return jdbcTemplate.query(sql, new ResumeDaoMapper(), applicantName + "%");
+    }
+
+    public Optional<Resume> getResumeById(Long id) {
+        String sql = "select * from resumes where id = ?";
+        Resume resume = jdbcTemplate.queryForObject(sql, new ResumeDaoMapper(), id);
+        return Optional.ofNullable(resume);
+    }
 }
