03e5e0ff9efad965c0c6983ac5cd10fa706df50f
Aisha Orozbekova <aisha.orozbekova@alatoo.edu.kg>
2025-03-23 00:52:00 +0600

    Added initial models
    * added models for ContactType and Category
    
    * added models for ContactInfo and EducationInfo
    
    * added models for User and Vacancy
    
    * added models for RespondedApplicant and Message
    
    * added models for Resume and WorkExperienceInfo
    
    * all done

---
+++ src/main/java/kg/attractor/models/Message.java
@@ -0,0 +1,27 @@
+package kg.attractor.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+
+public class Message {
+    private int id;
+    private String content;
+    private Timestamp timestamp;
+    private int respondedApplicantId;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Message{");
+        sb.append("id=").append(id);
+        sb.append(", content='").append(content).append('\'');
+        sb.append(", timestamp=").append(timestamp);
+        sb.append(", respondedApplicantId=").append(respondedApplicantId);
+        sb.append('}');
+        return sb.toString();
+    }
+}
