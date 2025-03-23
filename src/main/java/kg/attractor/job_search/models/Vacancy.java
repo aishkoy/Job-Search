d418c96012f65406d773be6ed6737f0c5e5e06a3
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-03-23 17:46:30 +0600

    fix: path has been changed

--- src/main/java/kg/attractor/job_search/models/Vacancy.java
+++ src/main/java/kg/attractor/job_search/models/Vacancy.java
@@ -1,4 +1,4 @@
-package kg.attractor.models;
+package kg.attractor.job_search.models;
 
 import lombok.Builder;
 import lombok.Data;
@@ -7,26 +7,32 @@ import java.sql.Timestamp;
 
 @Data
 @Builder
-public class Resume {
+public class Vacancy {
     private int id;
-    private int applicantId;
     private String name;
+    private String description;
     private int categoryId;
     private Float salary;
+    private int expFrom;
+    private int expTo;
     private boolean isActive;
-    private Timestamp createdData;
+    private int authorId;
+    private Timestamp createdDate;
     private Timestamp updateTime;
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Resume{");
+        final StringBuilder sb = new StringBuilder("Vacancy{");
         sb.append("id=").append(id);
-        sb.append(", applicantId=").append(applicantId);
         sb.append(", name='").append(name).append('\'');
+        sb.append(", description='").append(description).append('\'');
         sb.append(", categoryId=").append(categoryId);
         sb.append(", salary=").append(salary);
+        sb.append(", expFrom=").append(expFrom);
+        sb.append(", expTo=").append(expTo);
         sb.append(", isActive=").append(isActive);
-        sb.append(", createdData=").append(createdData);
+        sb.append(", authorId=").append(authorId);
+        sb.append(", createdDate=").append(createdDate);
         sb.append(", updateTime=").append(updateTime);
         sb.append('}');
         return sb.toString();
