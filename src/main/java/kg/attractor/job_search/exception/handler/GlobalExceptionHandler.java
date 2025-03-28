c79df37293e309c34f34b041e716e1161edf8071
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-03-28 14:37:52 +0600

    feat: global exception handler was created

---
+++ src/main/java/kg/attractor/job_search/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package kg.attractor.job_search.exception.handler;
+
+import kg.attractor.job_search.service.ErrorService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+    private final ErrorService errorService;
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseBody> handleNSEE(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(e), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponseBody> handleNSEE(NoSuchElementException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(e), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseBody> handleValidation(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorService.makeResponse(e.getBindingResult()), HttpStatus.BAD_REQUEST);
+    }
+}
