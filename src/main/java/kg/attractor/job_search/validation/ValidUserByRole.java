773cda805b5805352ea922d5b1a083375bc63bac
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-04-11 09:11:05 +0600

    feat: added custom validation to create users

---
+++ src/main/java/kg/attractor/job_search/validation/ValidUserByRole.java
@@ -0,0 +1,16 @@
+package kg.attractor.job_search.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import kg.attractor.job_search.validation.validator.UserRoleValidator;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UserRoleValidator.class)
+public @interface ValidUserByRole {
+    String message() default "Некорректные данные для выбранной роли";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
