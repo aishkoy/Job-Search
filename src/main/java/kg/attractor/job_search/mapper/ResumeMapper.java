60f1dc3e7fe006d946403375157c2fa5bf87a7bd
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-04-21 03:07:19 +0600

    refactor: optimized mapping

--- src/main/java/kg/attractor/job_search/mapper/ResumeMapper.java
+++ src/main/java/kg/attractor/job_search/mapper/ResumeMapper.java
@@ -1,14 +1,19 @@
 package kg.attractor.job_search.mapper;
+
 import kg.attractor.job_search.dto.resume.ResumeFormDto;
 import kg.attractor.job_search.dto.resume.ResumeDto;
 import kg.attractor.job_search.entity.Resume;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        uses = {EducationInfoMapper.class, WorkExperienceMapper.class, ContactInfoMapper.class},
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface ResumeMapper {
     ResumeDto toDto(Resume resume);
+
     ResumeDto toDto(ResumeFormDto resumeFormDto);
 
     @Mapping(target = "id", ignore = true)
