d24c30859077cfe98ea92e585995875a00db6346
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-04-21 15:33:31 +0600

    feat: added crud for work experience info

---
+++ src/main/java/kg/attractor/job_search/controller/WorkExperienceController.java
@@ -0,0 +1,109 @@
+package kg.attractor.job_search.controller;
+
+import jakarta.validation.Valid;
+import kg.attractor.job_search.dto.WorkExperienceInfoDto;
+import kg.attractor.job_search.service.WorkExperienceInfoService;
+import kg.attractor.job_search.service.ResumeService;
+import kg.attractor.job_search.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller("mvcWorkExperience")
+@RequestMapping("/resumes/{resumeId}/experiences")
+@RequiredArgsConstructor
+public class WorkExperienceController {
+    private final WorkExperienceInfoService workExperienceInfoService;
+    private final UserService userService;
+    private final ResumeService resumeService;
+
+    @GetMapping("/create")
+    public String showCreateExperienceModal(@PathVariable Long resumeId,
+                                            Model model
+    ) {
+        resumeService.getResumeById(resumeId, userService.getAuthId());
+
+        WorkExperienceInfoDto experienceDto = new WorkExperienceInfoDto();
+        experienceDto.setResumeId(resumeId);
+
+        model.addAttribute("mode", "create");
+        model.addAttribute("experienceDto", experienceDto);
+        model.addAttribute("action", "/resumes/" + resumeId + "/experiences");
+
+        return "info/experience";
+    }
+
+    @PostMapping
+    public String createExperience(@PathVariable Long resumeId,
+                                   @ModelAttribute("experienceDto") @Valid WorkExperienceInfoDto experienceDto,
+                                   BindingResult bindingResult,
+                                   Model model
+    ) {
+        resumeService.getResumeById(resumeId, userService.getAuthId());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("mode", "create");
+            model.addAttribute("action", "/resumes/" + resumeId + "/experiences");
+            return "info/experience";
+        }
+
+        workExperienceInfoService.createWorkExperience(experienceDto);
+
+        return "redirect:/resumes/" + resumeId + "/edit";
+    }
+
+    @GetMapping("/{experienceId}/edit")
+    public String showEditExperienceModal(@PathVariable Long resumeId,
+                                          @PathVariable Long experienceId,
+                                          Model model
+    ) {
+        resumeService.getResumeById(resumeId, userService.getAuthId());
+
+        WorkExperienceInfoDto experienceDto = workExperienceInfoService.getWorkExperienceById(experienceId);
+        experienceDto.setResumeId(resumeId);
+
+        model.addAttribute("mode", "edit");
+        model.addAttribute("experienceDto", experienceDto);
+        model.addAttribute("action", "/resumes/" + resumeId + "/experiences/" + experienceId + "/edit");
+
+        return "info/experience";
+    }
+
+    @PostMapping("/{experienceId}/edit")
+    public String editExperience(@PathVariable Long resumeId,
+                                 @PathVariable Long experienceId,
+                                 @ModelAttribute("experienceDto") @Valid WorkExperienceInfoDto experienceDto,
+                                 BindingResult bindingResult,
+                                 RedirectAttributes redirectAttributes,
+                                 Model model
+    ) {
+        resumeService.getResumeById(resumeId, userService.getAuthId());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("mode", "edit");
+            model.addAttribute("action", "/resumes/" + resumeId + "/experiences/" + experienceId + "/edit");
+            return "info/experience";
+        }
+
+        workExperienceInfoService.updateWorkExperienceInfo(experienceDto);
+
+        redirectAttributes.addFlashAttribute("successMessage", "Опыт работы успешно обновлен");
+        return "redirect:/resumes/" + resumeId + "/edit";
+    }
+
+    @PostMapping("/{experienceId}/delete")
+    public String deleteExperience(@PathVariable Long resumeId,
+                                   @PathVariable Long experienceId,
+                                   RedirectAttributes redirectAttributes
+    ) {
+        resumeService.getResumeById(resumeId, userService.getAuthId());
+
+        workExperienceInfoService.deleteWorkExperienceInfo(experienceId);
+
+        redirectAttributes.addFlashAttribute("successMessage", "Опыт работы успешно удален");
+        return "redirect:/resumes/" + resumeId + "/edit";
+    }
+}
--- src/main/java/kg/attractor/job_search/service/WorkExperienceInfoService.java
+++ src/main/java/kg/attractor/job_search/service/WorkExperienceInfoService.java
@@ -3,14 +3,9 @@ package kg.attractor.job_search.service;
 import kg.attractor.job_search.dto.WorkExperienceInfoDto;
 import org.springframework.http.HttpStatus;
 
-import java.util.List;
-
 public interface WorkExperienceInfoService {
     Long createWorkExperience(WorkExperienceInfoDto createWorkExperienceInfoDto);
-
-    List<WorkExperienceInfoDto> getWorkExperienceInfoByResumeId(Long resumeId);
-
-    Long updateWorkExperienceInfo(Long id, WorkExperienceInfoDto createWorkExperienceInfoDto);
+    Long updateWorkExperienceInfo(WorkExperienceInfoDto createWorkExperienceInfoDto);
 
     WorkExperienceInfoDto getWorkExperienceById(Long id);
 
