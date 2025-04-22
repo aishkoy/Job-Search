aaf3b638f4f4b8edd3e83bb6a9eb5a82e2540d2e
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-04-22 13:47:34 +0600

    feat: added vacancy/resume pagination

--- src/main/java/kg/attractor/job_search/controller/ProfileController.java
+++ src/main/java/kg/attractor/job_search/controller/ProfileController.java
@@ -5,11 +5,18 @@ import kg.attractor.job_search.dto.user.SimpleUserDto;
 import kg.attractor.job_search.dto.user.UserDto;
 import kg.attractor.job_search.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller("mvcProfile")
 @RequestMapping("profile")
@@ -17,9 +24,22 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProfileController {
     private final UserService userService;
 
+
     @GetMapping()
-    public String profile(Model model) {
-        model.addAttribute("user", userService.getAuthUser());
+    public String profile(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "3") int size,
+            Model model) {
+
+        UserDto user = userService.getAuthUser();
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        model.addAttribute("user", user);
+        model.addAttribute("size", size);
+
+        model.addAttribute("resumesPage", toPage(user.getResumes(), pageable));
+        model.addAttribute("vacanciesPage", toPage(user.getVacancies(), pageable));
+
         return "profile/profile";
     }
 
@@ -51,5 +71,16 @@ public class ProfileController {
 
         userService.updateUser(userService.getAuthId(), userDto);
         return "redirect:/profile";
+    }
+
+    private <T> Page<T> toPage(List<T> list, Pageable pageable) {
+        return new PageImpl<>(
+                list.stream()
+                        .skip(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .toList(),
+                pageable,
+                list.size()
+        );
     }
 }
