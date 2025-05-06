89e041661977ee3c85e922e437a21e21c9fdc4ce
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-07 00:08:26 +0600

    feat: add internationalization support with locale switching

---
+++ src/main/java/kg/attractor/job_search/config/LocaleConfig.java
@@ -0,0 +1,29 @@
+package kg.attractor.job_search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeChangeInterceptor() {
+        var loc = new LocaleChangeInterceptor();
+        loc.setParamName("lang");
+        return loc;
+    }
+}
