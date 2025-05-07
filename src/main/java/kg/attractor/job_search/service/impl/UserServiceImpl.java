28f4889ea6cfd7b059f1450141ee20ce75037835
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-07 20:43:10 +0600

    Feat: added translation for edit profile page

--- src/main/java/kg/attractor/job_search/service/impl/UserServiceImpl.java
+++ src/main/java/kg/attractor/job_search/service/impl/UserServiceImpl.java
@@ -37,7 +37,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.*;
 import java.util.function.Supplier;
 
