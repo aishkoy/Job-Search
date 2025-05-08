9db850e64915374e6d54eb66bf383937dfb940d1
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-08 19:17:22 +0600

    refactor: changed locale resolver

--- src/main/java/kg/attractor/job_search/config/LocaleConfig.java
+++ src/main/java/kg/attractor/job_search/config/LocaleConfig.java
@@ -5,15 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
-        return new SessionLocaleResolver();
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver("language");
+        cookieLocaleResolver.setDefaultLocale(new Locale("ru"));
+        cookieLocaleResolver.setCookieMaxAge(365*24*60*60);
+        return cookieLocaleResolver;
     }
 
     @Override
